dbd2dd31db6f614a1bfa9f6a9c4d2f879e313dcd
Johannes Maier <johb.maier@gmail.com>
2021-03-21 12:51:52 +0100

    Change custom MTGAP keys to use default shift states

--- users/kenranunderscore/kenranunderscore.c
+++ users/kenranunderscore/kenranunderscore.c
@@ -4,27 +4,27 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, SYMBOL, NUM_NAV, ADJUST);
 }
 
-uint16_t mtgap_alt_keymap[2][4] = {{ KC_COMM, KC_SLSH, KC_DOT, KC_COLN }, { KC_SCLN, KC_LABK, KC_RABK, KC_QUES }};
+/* uint16_t mtgap_alt_keymap[2][4] = {{ KC_COMM, KC_SLSH, KC_DOT, KC_COLN }, { KC_SCLN, KC_LABK, KC_RABK, KC_QUES }}; */
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case MTG_CMM ... MTG_CLN:
-      if (record->event.pressed) {
-        const uint8_t mod_state = get_mods();
-        const uint16_t key_index = keycode - MTG_CMM;
-        if (mod_state & MOD_MASK_SHIFT) {
-          unregister_mods(mod_state);
-          tap_code16(mtgap_alt_keymap[1][key_index]);
-          register_mods(mod_state);
-        } else {
-          tap_code16(mtgap_alt_keymap[0][key_index]);
-        }
-      }
-      return false;
-    default:
-      return true;
-  }
-}
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/*   switch (keycode) { */
+/*     case MTG_CMM ... MTG_CLN: */
+/*       if (record->event.pressed) { */
+/*         const uint8_t mod_state = get_mods(); */
+/*         const uint16_t key_index = keycode - MTG_CMM; */
+/*         if (mod_state & MOD_MASK_SHIFT) { */
+/*           unregister_mods(mod_state); */
+/*           tap_code16(mtgap_alt_keymap[1][key_index]); */
+/*           register_mods(mod_state); */
+/*         } else { */
+/*           tap_code16(mtgap_alt_keymap[0][key_index]); */
+/*         } */
+/*       } */
+/*       return false; */
+/*     default: */
+/*       return true; */
+/*   } */
+/* } */
 
 #ifdef COMBO_ENABLE
 
